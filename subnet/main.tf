17bbe614785769a99304becebd269b9302e195a0
subhambehera12 <162458185+subhambehera12@users.noreply.github.com>
2025-09-30 21:45:47 +0530

    Update main.tf

--- subnet/main.tf
+++ subnet/main.tf
@@ -1,1 +1,9 @@
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
 
+  tags = {
+    Name = var.public_subnet_name
+  }
+}
