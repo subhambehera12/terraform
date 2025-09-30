5669476330c181bab46b4ffa6965d4f69e9de1e3
subhambehera12 <162458185+subhambehera12@users.noreply.github.com>
2025-09-30 21:50:51 +0530

    Update var.tf

--- vpc/var.tf
+++ vpc/var.tf
@@ -1,1 +1,14 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
 
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name of VPC"
+  type        = string
+}
