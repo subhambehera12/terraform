fd2319e228683fcb9d219097fd810461461e79b6
subhambehera12 <162458185+subhambehera12@users.noreply.github.com>
2025-10-01 11:52:07 +0530

    Create main.tf

---
+++ Ec2/main.tf
@@ -0,0 +1,44 @@
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-sg"
+  }
+}
+
+resource "aws_instance" "this" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  key_name               = var.key_name
+
+  tags = {
+    Name = "my-ec2"
+  }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.this.public_ip
+}
